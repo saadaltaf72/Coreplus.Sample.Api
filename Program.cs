81fad9eee4926127c5eb7b03ccd2f5e8f2db3b75
Saad Altaf <Saad Altaf@DESKTOP-9HOJDD5>
2023-12-10 13:57:52 +0500

    Added CORS

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
 using Coreplus.Sample.Api.Endpoints.Practitioner;
 using Coreplus.Sample.Api.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<PractitionerService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:5173")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+
 var app = builder.Build();
+
+app.UseCors("AllowSpecificOrigin");
 
 var practitionerEndpoints = app.MapGroup("/practitioners");
 practitionerEndpoints.MapPractitionerEndpoints();
